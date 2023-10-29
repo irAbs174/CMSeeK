6e0b53beaf2b694fd61536f59947e21af0f2a653
48845 48845 <miror49735@sportrid.com>
2023-10-29 07:01:55 +0000

    block

--- forTest.go
+++ forTest.go
@@ -1,2 +1,11 @@
-package main ;import ("fmt")
-func main() {var num int;fmt.Print("Enter number: ");fmt.Scanln(&num);for i := 1; i < num; i++ {for j := 1; j < i; j ++ {;fmt.Print("*")};fmt.Println()}};
+package main 
+import ("fmt")
+
+func main(){
+	fmt.Print("HELLO")
+	fmt.Print("HELLO")
+	fmt.Print("HELLO")
+	fmt.Print("HELLO")
+	fmt.Print("HELLO")
+	fmt.Print("HELLO")
+}
